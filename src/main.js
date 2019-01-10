9995c9949c0cc5930367904843eb20b64655ded2
Morales <cmorales1@MATL2232CMORAL.local>
2019-01-09 19:41:04 -0500

    adding Vuex 'hello World'

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import store from './state/index';
 import router from './router';
 import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App),
+    store,
+    router,
+    render: h => h(App),
 }).$mount('#app');
