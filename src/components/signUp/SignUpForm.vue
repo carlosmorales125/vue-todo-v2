03ba239ff9bc473b8c71facc4fc64673012e7687
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-17 21:40:29 -0500

    added createUser action and wired it to the singupform

--- src/components/signUp/SignUpForm.vue
+++ src/components/signUp/SignUpForm.vue
@@ -3,32 +3,32 @@
         <div class="field">
             <label class="label">Name</label>
             <div class="control">
-                <input class="input" type="text" placeholder="Enter Your Name">
+                <input class="input" type="text" placeholder="Enter Your Name" v-model="name">
             </div>
         </div>
         <div class="field">
             <label class="label">Email</label>
             <div class="control">
-                <input class="input" type="email" placeholder="Enter Your Email">
+                <input class="input" type="email" placeholder="Enter Your Email" v-model="email">
             </div>
         </div>
         <div class="field">
             <label class="label">Password</label>
             <div class="control">
-                <input class="input" type="Password" placeholder="Enter Your Password">
+                <input class="input" type="Password" placeholder="Enter Your Password" v-model="password">
             </div>
         </div>
         <div class="field">
             <label class="label">Confirm Password</label>
             <div class="control">
-                <input class="input" type="Password" placeholder="Confirm Your Password">
+                <input class="input" type="Password" placeholder="Confirm Your Password" v-model="confirmPassword">
             </div>
         </div>
         <div class="field">
             <div class="control">
-                <buttton class="button is-link" @click.prevent="submitSignUpForm">
+                <button class="button is-link" @click.prevent="submitSignUpForm">
                     Sign Up
-                </buttton>
+                </button>
                 <router-link to="/login" class="button is-primary">
                     Go To Login
                 </router-link>
@@ -37,12 +37,30 @@
     </form>
 </template>
 <script>
+    import { mapActions } from 'vuex';
+
     export default {
         name: 'signupform',
         methods: {
+            ...mapActions({
+                createUser: 'createUser'
+            }),
             submitSignUpForm() {
-                // todo: make this work
+                this.createUser({
+                    name: this.name,
+                    email: this.email,
+                    password: this.password,
+                    confirmPassword: this.confirmPassword
+                });
             },
+        },
+        data() {
+            return {
+                name: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+            };
         },
     };
 </script>
--- src/state/profile/actions.js
+++ src/state/profile/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const actions = {
         changeName({ commit }, payload) {
             commit('changeName', payload);
@@ -5,6 +7,16 @@ const actions = {
         changeEmail({ commit }, payload) {
             commit('changeEmail', payload);
         },
+        createUser({ commit }, payload) {
+            axios.post('http://localhost:3000/api/users/createuser', payload)
+                .then(resp => {
+                    console.log(resp);
+                    // if the response is good, I'd like to route the user to either login or todo page.
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
 };
 
 export default actions;
