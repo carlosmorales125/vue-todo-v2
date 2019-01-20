69eb3b6ef9b23efb536a37c763435be8825ac4d7
Morales, Carlos <Carlos.Rey.Morales@gmail.com>
2019-01-20 15:49:11 -0500

    making changes to createuser

--- src/state/profile/mutations.js
+++ src/state/profile/mutations.js
@@ -5,7 +5,8 @@ const mutations = {
         changeEmail(state, email) {
             state.user.email = email;
         },
-        createUser(state, { name, email }) {
+        createUser(state, { _id, name, email }) {
+            state.user.id = _id;
             state.user.name = name;
             state.user.email = email;
         }
